82bd87440cd50bf5900dacd48201cef940b829c6
ThanaCS <Thana_cs@hotmail.com>
2021-12-26 19:41:28 +0300

    Replace color picker library

--- app/src/main/java/com/example/pencil/ui/MakerFragment.kt
+++ app/src/main/java/com/example/pencil/ui/MakerFragment.kt
@@ -1,24 +1,20 @@
-package com.example.pencil
+package com.example.pencil.ui
 
-import android.graphics.Color
 import android.graphics.Paint
 import android.os.Bundle
 import android.view.View
-import androidx.core.content.res.ResourcesCompat
-import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.pencil.CanvasView
+import com.example.pencil.R
 import com.example.pencil.databinding.FragmentMakerBinding
-import com.example.pencil.ui.ToolAdapter
-import com.example.pencil.ui.ToolClickListeners
 import com.example.pencil.ui.common.BaseFragment
 import com.example.pencil.ui.common.viewBinding
 import com.example.pencil.ui.data.CategoryType
 import com.example.pencil.ui.data.Tool
-import com.example.pencil.ui.data.getToolList
-import com.azeesoft.lib.colorpicker.ColorPickerDialog
 import com.example.pencil.ui.data.getBackgroundList
+import com.example.pencil.ui.data.getToolList
 import com.example.pencil.util.getIcon
-import java.lang.Exception
-
+import com.skydoves.colorpickerview.ColorPickerDialog
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener
 
 class MakerFragment : BaseFragment(R.layout.fragment_maker) {
 
@@ -28,7 +24,6 @@ class MakerFragment : BaseFragment(R.layout.fragment_maker) {
     private lateinit var toolAdapter: ToolAdapter
     lateinit var canvas: CanvasView
     private lateinit var paint: Paint
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -42,26 +37,17 @@ class MakerFragment : BaseFragment(R.layout.fragment_maker) {
     private fun collapse(view: View, collapseView: View) {
         view.visibility = View.INVISIBLE
         collapseView.visibility = View.INVISIBLE
-
     }
 
     private fun initAdapter() {
-
         list.addAll(getToolList(requireContext()))
-        binding.recyclerview.layoutManager =
-            LinearLayoutManager(requireActivity(), LinearLayoutManager.HORIZONTAL, false)
 
         toolAdapter = ToolAdapter(object : ToolClickListeners {
             override fun getTool(tool: Tool) {
-
-                if (tool.isFormat) {
-                    onFormatClicked(tool)
-                } else {
-                    onToolBarClicked(tool)
-                }
+                if (tool.isFormat) onFormatClicked(tool) else onToolBarClicked(tool)
             }
-
         })
+
         toolAdapter.differ.submitList(list)
         binding.recyclerview.adapter = toolAdapter
     }
@@ -70,17 +56,12 @@ class MakerFragment : BaseFragment(R.layout.fragment_maker) {
         when (tool.type) {
 
             CategoryType.TEXT -> {
-
             }
-            CategoryType.BACKGROUND -> {
-                when (tool.id) {
-                    1 -> binding.layout.background =
-                        resources.getIcon(R.drawable.paper_1)
-                    2 -> binding.layout.background = resources.getIcon(R.color.white)
-                    3 -> binding.layout.background = resources.getIcon(R.color.black)
-                    4 -> binding.layout.background =
-                        resources.getIcon(R.color.off_white)
-                }
+            CategoryType.BACKGROUND -> when (tool.id) {
+                1 -> binding.layout.background = resources.getIcon(R.drawable.paper_1)
+                2 -> binding.layout.background = resources.getIcon(R.color.white)
+                3 -> binding.layout.background = resources.getIcon(R.color.black)
+                4 -> binding.layout.background = resources.getIcon(R.color.off_white)
             }
             CategoryType.BACK -> {
                 updateList(getToolList(requireContext()))
@@ -99,20 +80,7 @@ class MakerFragment : BaseFragment(R.layout.fragment_maker) {
             }
             CategoryType.COLOR -> {
                 canvas.removeEraser()
-                val colorPickerDialog = ColorPickerDialog.createColorPickerDialog(
-                    requireContext(),
-                    ColorPickerDialog.DARK_THEME
-                )
-                colorPickerDialog.show()
-                colorPickerDialog.setOnColorPickedListener { _, hex ->
-                    try {
-                        paint.color = Color.parseColor(hex)
-                    } catch (e: Exception) {
-                        paint.color =
-                            ResourcesCompat.getColor(resources, R.color.black, null)
-                    }
-
-                }
+                getColorPicker()
             }
             CategoryType.TEXT -> {
 
@@ -127,7 +95,6 @@ class MakerFragment : BaseFragment(R.layout.fragment_maker) {
                 canvas.eraser()
                 sliderWidth()
             }
-
             else -> {
             }
         }
@@ -136,6 +103,7 @@ class MakerFragment : BaseFragment(R.layout.fragment_maker) {
     private fun sliderWidth() {
         binding.slider.visibility = View.VISIBLE
         binding.cancel.visibility = View.VISIBLE
+        binding.slider.value = paint.strokeWidth
         binding.slider.addOnChangeListener { _, value, _ ->
             paint.strokeWidth = value
 
@@ -150,5 +118,23 @@ class MakerFragment : BaseFragment(R.layout.fragment_maker) {
         list.clear()
         list.addAll(newList)
         toolAdapter.notifyDataSetChanged()
+    }
+
+    private fun getColorPicker() {
+        ColorPickerDialog.Builder(requireContext())
+            .setTitle("Color Picker")
+            .setPreferenceName("MyColorPickerDialog")
+            .setPositiveButton("Ok",
+                ColorEnvelopeListener { envelope, _ ->
+                    paint.color = envelope.color
+
+                })
+            .setNegativeButton(
+                "Cancel"
+            ) { dialogInterface, _ -> dialogInterface.dismiss() }
+            .attachAlphaSlideBar(true)
+            .attachBrightnessSlideBar(true)
+            .setBottomSpace(12)
+            .show()
     }
 }
